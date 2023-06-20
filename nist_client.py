e4969594c8c956ac8f18603ab73fcf2beb0bbab8
Gianluca Pericoli <gianluca.pericoli@INTERLOGICA.LAN>
2023-06-20 12:37:34 +0200

    Change 1 week

--- nist_client.py
+++ nist_client.py
@@ -7,7 +7,7 @@ class NistClient:
     @staticmethod
     def get_nist_cves():
         today = datetime.now()
-        mod_start_date = (today - timedelta(days=1)).strftime("%Y-%m-%dT00:00:00.000")
+        mod_start_date = (today - timedelta(weeks=1)).strftime("%Y-%m-%dT00:00:00.000")
         mod_end_date = today.strftime("%Y-%m-%dT00:00:00.000")
         url = config.NIST_API.format(mod_start_date, mod_end_date)
 
